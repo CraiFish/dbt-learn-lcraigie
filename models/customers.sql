051ab786cfecefc3ea8f9ed48dbc7736d7d5a3f5
Lauren Craigie <lauren@fishtownanalytics.com>
2020-10-14 22:33:20 +0000

    Day1

--- models/customers.sql
+++ models/customers.sql
@@ -16,14 +16,7 @@ with customers as (
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+select * from {{ ref('orders')}}
 
 ),
 
